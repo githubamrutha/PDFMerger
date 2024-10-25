aeb906a0d2e21895725dd551d65a9011e8f58f78
Amrutha Reddy <amruthachikki19@gmail.com>
2024-10-25 18:57:15 +0530

    index.html 2

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const upload = multer({ dest: 'uploads/' })
 
 
 app.use('/static',express.static('public'))
-const port = 3000
+const port = process.env.PORT || 3000;
+
 // const port = process.env.PORT || 3000;
 // app.listen(port, () => {
 //   console.log(`Server running on port ${port}`);
